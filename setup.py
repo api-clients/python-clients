8ec8dbd556ebbfff015d36391d1fcd14c4cc6e8d
Egor Urvanov <hedgehogues@bk.ru>
2019-09-29 19:44:36 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_reqs = parse_requirements('./requirements')
+print(install_reqs)
+
+setuptools.setup(
+    name="python-clients",
+    version="0.0.1",
+    author="Egor Urvanov",
+    author_email="hedgehogues@bk.ru",
+    description="This library implements wrapper for different python interfaces",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hedgehogues/python-clients",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_reqs,
+)
