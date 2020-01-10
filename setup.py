0373c499c69a5014559212c974f319c472857e5e
Egor <hedgehogues@bk.ru>
2020-01-10 09:28:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ print(install_reqs)
 
 setuptools.setup(
     name="python-clients",
-    version="0.10.1",
+    version="0.10.3",
     author="Egor Urvanov",
     author_email="hedgehogues@bk.ru",
     description="This library implements wrapper for different python interfaces",
